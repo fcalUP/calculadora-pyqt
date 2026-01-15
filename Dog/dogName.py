0730c861faeee94e10d38cf5edb83e78574a6bcf
fcalUP <54124933+fcalUP@users.noreply.github.com>
2026-01-15 17:26:09 -0600

    Se escribieron las instrucciones del reto de DogName

---
+++ Dog/dogName.py
@@ -0,0 +1,2 @@
+# crear un programa que le pregunte al usuario qué pais le gusta más y qué comida le gusta más y uniendo
+# uniendo esas dos variables imprima un mensaje que diga lo siguiete: "Tu pero se llamará (pais) (comida)"
