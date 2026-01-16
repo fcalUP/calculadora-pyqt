c09f1bc99156a378732476c330c6f3f7562dfdbe
fcalUP <54124933+fcalUP@users.noreply.github.com>
2026-01-16 12:30:59 -0600

    url update

--- Propina/propina.py
+++ Propina/propina.py
@@ -1,17 +1,17 @@
-cuenta = float(input("Cuanto les cobraron?"))
-personas = int(input("Cuantas personas van a pagar?"))
-if personas <2 and cuenta < 200:
-  print("Y qué? no se querían tomar una cervecita?")
-  propina = float(input("Cuanto van a dejar de propina?\n 10, 15 o 20%"))
-  cuentaPersonal = (cuenta/personas) *(1-(propina/100))
-  print(f"Cada persona debe pagar: {cuentaPersonal}")
+cuenta = float(input("Cuanto les cobraron?\n"))
+personas = int(input("Cuantas personas van a pagar?\n"))
+if personas < 2 and cuenta < 200:
+    print("\n****** Y qué? no se querían tomar una cervecita? ******\n")
+    propina = float(input("Cuanto van a dejar de propina?\n 10, 15 o 20%"))
+    cuentaPersonal = (cuenta/personas) * (1-(propina/100))
+    print(f"Cada persona debe pagar: {cuentaPersonal} pesos")
 
 elif personas > 2 and cuenta > 500:
-  print("Llamen a la patruylla, esto es un robo!")
-  propina = float(input("Cuanto van a dejar de propina?\n 10, 15 o 20%"))
-  cuentaPersonal = (cuenta/personas) *(1-(propina/100))
-  print(f"Cada persona debe pagar: {cuentaPersonal}")
+    print("Llamen a la patruylla, esto es un robo!")
+    propina = float(input("Cuanto van a dejar de propina? 10, 15 o 20%\n"))
+    cuentaPersonal = round((cuenta/personas) * (1-(propina/100)), 2)
+    print(f"Cada persona debe pagar: {cuentaPersonal}")
 else:
-  propina = float(input("Cuanto van a dejar de propina?\n 10, 15 o 20%"))
-  cuentaPersonal = (cuenta/personas) *(1-(propina/100))
-  print(f"Cada persona debe pagar: {cuentaPersonal}")
+    propina = float(input("Cuanto van a dejar de propina? 10, 15 o 20%\n"))
+    cuentaPersonal = (cuenta/personas) * (1-(propina/100))
+    print(f"Cada persona debe pagar: {round(cuentaPersonal, 2)}\n")
