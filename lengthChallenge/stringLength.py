6c209e3fa26cdc197b15d6da9a8f2101736db5bb
fcalUP <54124933+fcalUP@users.noreply.github.com>
2026-01-20 23:01:27 -0600

    Se quito un comentario

--- lengthChallenge/stringLength.py
+++ lengthChallenge/stringLength.py
@@ -1,22 +1,21 @@
-#Challenge: String Length
-#Da la longitud de una cadena de las variables despues del print
-#Da la longitud de la cadena que meta el usuario en una sola linea
+# Challenge: String Length
+# Da la longitud de una cadena de las variables despues del print
 
 
-cadena1="Hola Python"
-cadena2="Esternocleidomastoideo"
-cadena3=123456789
+cadena1 = "Hola Python"
+cadena2 = "Esternocleidomastoideo"
+cadena3 = 123456789
 
-print("",cadena1,"\n", cadena2,"\n", cadena3)
+print("", cadena1, "\n", cadena2, "\n", cadena3)
 
-#Challenge: Variables
-#Tenemos 2 variables, glass1 y glass2. glass1 contiene leche y glass2 contiene jugo.
-#Escribe 3 líneas de código para intercambiar el contenido de las variables.
-#No tienes permitido escribir las palabras 'milk' ni 'juice'.
-#Solo puedes usar variables para resolver este ejercicio. Al finalizar imprime
-#las variables para verificar que el contenido ha sido intercambiado.
+# Challenge: Variables
+# Tenemos 2 variables, glass1 y glass2. glass1 contiene leche y glass2 contiene jugo.
+# Escribe 3 líneas de código para intercambiar el contenido de las variables.
+# No tienes permitido escribir las palabras 'milk' ni 'juice'.
+# Solo puedes usar variables para resolver este ejercicio. Al finalizar imprime
+# las variables para verificar que el contenido ha sido intercambiado.
 
 glass1 = "milk"
 glass2 = "juice"
 
-print("Before Swap: \nglass1:", glass1,"\nglass2:" , glass2)
+print("Before Swap: \nglass1:", glass1, "\nglass2:", glass2)
